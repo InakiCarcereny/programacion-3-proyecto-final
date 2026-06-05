acd3caa0c0db68c8d718ac221342d091b3f9b559
inaki carcereny <inakicarcereny.dev@gmail.com>
2026-06-05 16:51:22 -0300

    feat: implement seed function and update main execution flow

--- app.ts
+++ app.ts
@@ -1,4 +1,13 @@
 import { Server } from "./core/server";
+import { seed } from "./seed";
 
-const server = new Server();
-server.listen();
+async function main(): Promise<void> {
+  if (process.env.NODE_ENV === "development") {
+    await seed();
+  }
+
+  const server = new Server();
+  server.listen();
+}
+
+main();
---
+++ seed.ts
@@ -0,0 +1,10 @@
+// import { Product } from './models';
+
+// Esta función se va a usar para insertar datos de prueba en la base de datos cuando se inicie el servidor. Cuando tengamos los modelos definidos, venimos aca y hacemos toda la carga de productos, etc.
+
+export async function seed(): Promise<void> {
+  // await Product.bulkCreate([
+  //   { name: 'Producto 1', price: 100, stock: 10 },
+  //   { name: 'Producto 2', price: 200, stock: 5 },
+  // ]);
+}
