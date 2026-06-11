a98ff1b135893020edc3119e9ec3bed0cd9109cb
JoaquinMarcilese <joaquimarci@gmail.com>
2026-06-11 12:35:06 +0000

    feat: agrego validaciones de middleware

--- routes/product.routes.ts
+++ routes/product.routes.ts
@@ -6,14 +6,16 @@ import {
   updateProduct,
   deleteProduct,
 } from "../controllers/product.controller";
+import { validateProduct } from "../middlewares/product.validator";
+
 import upload from "../middlewares/upload.middleware";
 
 const router: IRouter = Router();
 
 router.get("/", getProducts);
 router.get("/:id", getProductById);
-router.post("/", upload.single("image"), createProduct);
-router.put("/:id", upload.single("image"), updateProduct);
+router.post("/", upload.single("image"), validateProduct, createProduct);
+router.put("/:id", upload.single("image"), validateProduct, updateProduct);
 router.delete("/:id", deleteProduct);
 
 export default router;
