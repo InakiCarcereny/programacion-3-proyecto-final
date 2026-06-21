39a835246faea360bdea6a373af8653667c10152
inaki carcereny <inakicarcereny.dev@gmail.com>
2026-06-21 20:50:25 -0300

    feat: add SSL configuration for Sequelize database connection

--- lib/db/db.config.ts
+++ lib/db/db.config.ts
@@ -15,6 +15,12 @@ const sequelize = new Sequelize({
     timestamps: true,
     underscored: true,
   },
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
 });
 
 export default sequelize;
