eae5a4ce41fafcf8569839beb1e18d0ee5cc5c6c
ValentinDePascale <valentindp9@gmail.com>
2026-07-18 16:57:53 -0300

    feat: add CRUD to users controller

--- backend/controllers/users.controller.ts
+++ backend/controllers/users.controller.ts
@@ -1,2 +1,83 @@
-const mensaje: string = "Hola";
-console.log(mensaje);
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { Request, Response, NextFunction } from "express";
+//import { User } from "../models";
+
+export async function getUsers(
+  req: Request,
+  res: Response,
+  next: NextFunction,
+): Promise<void> {
+  //   try {
+  //     const users = await User.findAllUsers();
+  //     res.json(users);
+  //   } catch (error) {
+  //     next(error);
+  //   }
+}
+
+export async function getUserById(
+  req: Request,
+  res: Response,
+  next: NextFunction,
+): Promise<void> {
+  //   try {
+  //     const id = Number(req.params.id);
+  //     const user = await User.findUserById(id);
+  //     if (!user) {
+  //       res.status(404).json({ error: "User not found" });
+  //       return;
+  //     }
+  //     res.json(user);
+  //   } catch (error) {
+  //     next(error);
+  //   }
+}
+
+export async function createUser(
+  req: Request,
+  res: Response,
+  next: NextFunction,
+): Promise<void> {
+  //   try {
+  //     const user = await User.createUser(req.body);
+  //     res.status(201).json(user);
+  //   } catch (error) {
+  //     next(error);
+  //   }
+}
+
+export async function updateUser(
+  req: Request,
+  res: Response,
+  next: NextFunction,
+): Promise<void> {
+  //   try {
+  //     const id = Number(req.params.id);
+  //     const user = await User.updateUser(id, req.body);
+  //     if (!user) {
+  //       res.status(404).json({ error: "User not found" });
+  //       return;
+  //     }
+  //     res.json({ message: "User updated successfully", user });
+  //   } catch (error) {
+  //     next(error);
+  //   }
+}
+
+export async function deleteUser(
+  req: Request,
+  res: Response,
+  next: NextFunction,
+): Promise<void> {
+  //   try {
+  //     const id = Number(req.params.id);
+  //     const deleted = await User.deleteUser(id);
+  //     if (!deleted) {
+  //       res.status(404).json({ error: "User not found" });
+  //       return;
+  //     }
+  //     res.status(200).json({ message: "User deleted successfully" });
+  //   } catch (error) {
+  //     next(error);
+  //   }
+}
