56f2fe79baf780ef2ea20f550f339d3a615ae688
inaki carcereny <inakicarcereny.dev@gmail.com>
2026-07-24 13:23:32 -0300

    feat: implement Dashboard Layout components and styles

--- frontend/src/components/header/Header.tsx
+++ frontend/src/components/header/Header.tsx
@@ -1,7 +1,57 @@
-export function Header() {
+import { Search } from "lucide-react";
+import { useState, type JSX } from "react";
+import { useAuth } from "../../context/AuthContext";
+
+import "./Header.css";
+
+export function Header(): JSX.Element {
+  const { user } = useAuth();
+  const [focused, setFocused] = useState(false);
+
+  const initials =
+    `${user?.profile?.firstName?.[0] ?? ""}${user?.profile?.lastName?.[0] ?? ""}`.toUpperCase();
+
   return (
-    <header>
-      <h1>My Application</h1>
+    <header className="header">
+      <div
+        className="input-container"
+        style={{
+          border: `1px solid ${focused ? "#004ac6" : "#ccc"}`,
+          color: focused ? "#004ac6" : "#ccc",
+        }}
+      >
+        <Search size={20} />
+
+        <input
+          id="search"
+          name="search"
+          className="input"
+          type="text"
+          placeholder="Buscar movimientos, productos, categorias..."
+          onFocus={() => setFocused(true)}
+          onBlur={() => setFocused(false)}
+        />
+      </div>
+
+      <div className="user-container">
+        {user?.profile?.avatarUrl ? (
+          <img
+            src={user.profile.avatarUrl}
+            alt={`${user.profile.firstName} ${user.profile.lastName} avatar`}
+            className="user-avatar"
+          />
+        ) : (
+          <div className="user-avatar-fallback">{initials}</div>
+        )}
+
+        <div className="user-info">
+          <span className="user-name">
+            {user?.profile?.firstName} {user?.profile?.lastName}
+          </span>
+
+          <span className="user-role">{user?.role.toUpperCase()}</span>
+        </div>
+      </div>
     </header>
   );
 }
