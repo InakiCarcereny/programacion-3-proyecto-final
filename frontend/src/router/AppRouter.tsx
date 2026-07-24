393405d327d0135d6f508a97521077e47ea6714b
inaki carcereny <inakicarcereny.dev@gmail.com>
2026-07-24 13:21:36 -0300

    feat: implement DashboardLayout with CSS styling and integrate into AppRouter

--- frontend/src/router/AppRouter.tsx
+++ frontend/src/router/AppRouter.tsx
@@ -8,27 +8,30 @@ import MovementsPage from "../pages/movements/MovementsPage";
 import UsersPage from "../pages/users/UsersPage";
 import ProfilePage from "../pages/profile/ProfilePage";
 import ProtectedRoute from "./ProtectedRoute";
+import { DashboardLayout } from "../layouts/dashboard-layout/DashboardLayout";
 import type { JSX } from "react/jsx-runtime";
 
 function AppRouter(): JSX.Element {
   return (
     <Routes>
       {/* Public Routes */}
-      <Route path="/" element={<Navigate to="/login" />} />
-      <Route path="/login" element={<LoginPage />} />
-      <Route path="/register" element={<RegisterPage />} />
+      <Route path="/" element={<Navigate to="/iniciar-sesion" />} />
+      <Route path="/iniciar-sesion" element={<LoginPage />} />
+      <Route path="/registro" element={<RegisterPage />} />
 
       {/* Protected Routes */}
       <Route element={<ProtectedRoute />}>
-        <Route path="/dashboard" element={<DashboardPage />} />
-        <Route path="/products" element={<ProductsPage />} />
-        <Route path="/categories" element={<CategoriesPage />} />
-        <Route path="/movements" element={<MovementsPage />} />
-        <Route path="/profile" element={<ProfilePage />} />
+        <Route element={<DashboardLayout />}>
+          <Route path="/dashboard" element={<DashboardPage />} />
+          <Route path="/productos" element={<ProductsPage />} />
+          <Route path="/categorias" element={<CategoriesPage />} />
+          <Route path="/movimientos" element={<MovementsPage />} />
+          <Route path="/perfil" element={<ProfilePage />} />
 
-        {/* Only admin */}
-        <Route element={<ProtectedRoute role="admin" />}>
-          <Route path="/users" element={<UsersPage />} />
+          {/* Only admin */}
+          <Route element={<ProtectedRoute role="admin" />}>
+            <Route path="/usuarios" element={<UsersPage />} />
+          </Route>
         </Route>
       </Route>
 
