6b20f3e2032e160908d1c430b979797ae78843e8
inaki carcereny <inakicarcereny.dev@gmail.com>
2026-07-24 13:19:40 -0300

    feat: wrap App component with ModalProvider for improved modal state management

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,12 +4,15 @@ import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.tsx";
 import { AuthProvider } from "./context/AuthContext.tsx";
+import { ModalProvider } from "./context/ModalContext.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
       <AuthProvider>
-        <App />
+        <ModalProvider>
+          <App />
+        </ModalProvider>
       </AuthProvider>
     </BrowserRouter>
   </StrictMode>,
